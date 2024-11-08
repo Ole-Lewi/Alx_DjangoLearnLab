2b529861a43cb08c282399c6cbb4b6794de1018f
Ole-Lewi <lewisino666@gmail.com>
2024-11-08 22:51:18 +0300

    initial commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -10,3 +10,13 @@ urlpatterns = [
     # URL pattern for the class-based view that shows details of a specific library
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login')
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout')
+    path('register/',auth_views.RegisterView.as_view(template_name='relationship_app/register.html'), name='register')
+]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -12,4 +12,23 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
- ["relationship_app/library_detail.html"]
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import UserCreationForm,AuthenticatioForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log in the user after registration
+            messages.success(request, "Registration successful!")
+            return redirect('list_books')
+        else:
+            messages.error(request, "Registration failed. Please check the form.")
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
