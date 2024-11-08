38b895523dfb7a9664bfcd371722732bfe7f3a81
Ole-Lewi <lewisino666@gmail.com>
2024-11-08 11:26:23 +0300

    initial commit

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,7 @@
 # relationship_app/urls.py
 from django.urls import path
 from . import views
+from .views import list_books
 
 urlpatterns = [
     # URL pattern for the function-based view that lists all books
