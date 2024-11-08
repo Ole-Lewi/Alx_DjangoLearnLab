b7b23d992fbe2dbc6928bcf6eb56bfd57b07c6c9
Ole-Lewi <lewisino666@gmail.com>
2024-11-08 22:58:11 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -18,5 +18,5 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login')
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout')
-    path('register/',view.Register(template_name='relationship_app/register.html'), name='register')
+    path('register/',view.register(template_name='relationship_app/register.html'), name='register')
 ]
