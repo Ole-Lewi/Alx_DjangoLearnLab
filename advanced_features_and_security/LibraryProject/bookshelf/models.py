a8a33e46d2c2ef21574e87b50dfad423da205905
Ole-Lewi <lewisino666@gmail.com>
2024-11-12 00:32:10 +0300

    initial commit

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -7,3 +7,37 @@ class Book(models.Model):
 
     def _str_(self):
         return f"({self.title} by {self.author}, {self.publication_year})"
+from django.contrib.auth.models import AbstractUser
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=true, blank=true)
+    profile_photo = models.ImageField(upload_to='profile_photo/',null=True, blank=True)
+
+# users/models.py
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, date_of_birth, profile_photo=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth, profile_photo=profile_photo, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, date_of_birth, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, date_of_birth, password=password, **extra_fields)
+
+objects = CustomUserManager()
+
+from django.conf import settings
+class SomeModel(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
