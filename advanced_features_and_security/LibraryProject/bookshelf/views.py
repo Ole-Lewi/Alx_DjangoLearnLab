ac14d78fcb9929fbac5be385ca9c0f21483bbb12
Ole-Lewi <lewisino666@gmail.com>
2024-11-13 10:16:23 +0300

    initial commit

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,33 +1,33 @@
 from django.shortcuts import render
-
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Post
+from .models import Book
 
 @permission_required('your_app.can_view', raise_exception=True)
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'post_list.html', {'posts': posts})
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
 
 @permission_required('your_app.can_create', raise_exception=True)
-def post_create(request):
+def book_create(request):
     if request.method == 'POST':
         # Handle creation logic here
         pass
-    return render(request, 'post_create.html')
+    return render(request, 'book_create.html')
 
 @permission_required('your_app.can_edit', raise_exception=True)
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
         # Handle edit logic here
         pass
-    return render(request, 'post_edit.html', {'post': post})
+    return render(request, 'book_edit.html', {'book': book})
 
 @permission_required('your_app.can_delete', raise_exception=True)
-def post_delete(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
-        post.delete()
-        return redirect('post_list')
-    return render(request, 'post_confirm_delete.html', {'post': post})
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'book_confirm_delete.html', {'book': book})
+
