312f5f65760d5b7a3345ae7af1c841426dae2591
Ole-Lewi <lewisino666@gmail.com>
2024-12-07 00:52:56 +0300

    initial commit

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import register_view, profile_view, CustomLoginView, CustomLogoutView
+
+urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register_view, name='register'),
+    path('profile/', profile_view, name='profile'),
+]
