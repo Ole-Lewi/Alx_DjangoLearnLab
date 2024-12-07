9df04a021c636da8d40971d37f8b978dbd5e8c67
Ole-Lewi <lewisino666@gmail.com>
2024-12-07 12:16:01 +0300

    initial commit

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('books/add/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
