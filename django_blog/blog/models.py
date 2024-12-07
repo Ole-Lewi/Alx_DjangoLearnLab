0834d71bed8242142545ad95fd3d3e21309f98ff
Ole-Lewi <lewisino666@gmail.com>
2024-12-07 12:12:06 +0300

    initial commit

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -8,9 +8,14 @@ class Post(models.Model):
     published_date = models.DateTimeField(auto_now_add=True)
   
     author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='post')
+    created_at = models.DateTimeField(auto_add_now = True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def _str_ (self):
         return(self.title)
+    
+    def get_absolute_url(self):
+        return reverse ('post_detail', kwargs={'pk':self.pk})
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -56,3 +56,53 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save,sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+
+# ListView: Display all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'  # Template name
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+# DetailView: Display a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+# CreateView: Allow logged-in users to create a post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# UpdateView: Allow post authors to edit their posts
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# DeleteView: Allow post authors to delete their posts
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/posts/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
