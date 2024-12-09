634841937abd053a12647a923718dd1657b5cc1c
Ole-Lewi <lewisino666@gmail.com>
2024-12-09 21:04:45 +0300

    initial

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
     path('search/', search_posts, name='search-posts'),
-    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(model=Post), name='posts-by-tag'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),
 ]
