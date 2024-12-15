65a2694f58907d3fd6005589d463fd26fe01979e
Ole-Lewi <lewisino666@gmail.com>
2024-12-15 23:13:38 +0300

    initiate

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,6 +4,6 @@ from .views import RegisterView, LoginView, FeedView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name ='register'),
     path('login/', LoginView.as_view(), name = 'login'),
-    path('/follow/<int:user_id>', FeedView.as_view, name='follow'),
+    path('/follow/<int:user_id/>', FeedView.as_view, name='follow'),
     path('/unfollow/<int:user_id/>', name='unfollow')
 ]
