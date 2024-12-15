7b282cb270f569555a8c5d8866287b029d3dee59
Ole-Lewi <lewisino666@gmail.com>
2024-12-15 23:12:58 +0300

    initiate

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name ='register'),
     path('login/', LoginView.as_view(), name = 'login'),
     path('/follow/<int:user_id>', FeedView.as_view, name='follow'),
-    path('/unfollow/<int:user_id>', name='unfollow')
+    path('/unfollow/<int:user_id/>', name='unfollow')
 ]
