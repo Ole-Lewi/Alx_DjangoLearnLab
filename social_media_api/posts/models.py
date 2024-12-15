a93203a47b5dc9cbc5efd8fb588af78756d6f337
Ole-Lewi <lewisino666@gmail.com>
2024-12-15 12:33:24 +0300

    initial commit

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth import User
 class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.TextField()
-    created_at = models.DateField
-    updated_at = models.DateField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
